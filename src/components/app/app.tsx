729c2c4f5edd9e91c5bec32e35844e88d9b7455c
Svetlana Podgainova <spodgainova@gmail.com>
2024-11-06 16:13:31 +0300

    feat start rotes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,48 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <AppHeader />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='*' element={<Feed />} />
+
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+
+      <Route
+        path='/feed/:number'
+        element={<Modal title='' onClose={} children={<OrderInfo />} />}
+      />
+      <Route
+        path='/ingredients/:id'
+        element={<Modal title='' onClose={} children={<IngredientDetails />} />}
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={<Modal title='' onClose={} children={<OrderInfo />} />}
+      />
+    </Routes>
   </div>
 );
 
