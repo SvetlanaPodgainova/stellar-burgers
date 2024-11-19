4020853475ecbe64c0863287f5dffde6bb03aeec
Svetlana Podgainova <spodgainova@gmail.com>
2024-11-19 12:44:00 +0300

    fix

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,8 +1,7 @@
 import { ProfileUI } from '@ui-pages';
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import { getIsLoading, getUser } from '../../services/user/slice';
-import { useDispatch } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
 import { updateUser } from '../../services/user/actions';
 import { Preloader } from '@ui';
 
